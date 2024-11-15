8290e2350a5ec418093e8f8aca9a8a103a4a95ee
RafalW3bCraft <seb.baadi@hotmail.com>
2024-11-15 16:28:38 +0530

    initilize mod-3

--- bot-music-send.py
+++ bot-music-send.py
@@ -2,7 +2,7 @@ import requests
 import json
 import time
 
-api_token = "7926352569:AAFp9rQSEWf82zpaut_DQhNBbEeFsBYOdVI"
+api_token = "7926352569:AAFp9rQSEWf82zpaut_DQhNBbEeFs"
 url = f"https://api.telegram.org/bot{api_token}/sendAudio"
 
 audio_n_cap = [
